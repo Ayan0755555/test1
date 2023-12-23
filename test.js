eeb503f216ca95824edf85217a4e1fd04d9a414b
AYAN <138348654+Ayan0755555@users.noreply.github.com>
2023-12-23 19:36:39 +0530

    test1

---
+++ test.js
@@ -0,0 +1,13 @@
+function touch(){
+    window.location.href="http://127.0.0.1:5500/details.html"
+}
+function touched(){
+    window.location.href="http://127.0.0.1:5500/details2.html"
+}
+function touchedd(){
+    window.location.href="http://127.0.0.1:5500/details3.html"
+}
+
+function bookseat(){
+    window.location.href="http://127.0.0.1:5500/booking.html"
+}
